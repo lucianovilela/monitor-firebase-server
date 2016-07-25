d76dd2f0799b59ed8349826e053d82c68f62ef3d
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2016-07-25 18:44:50 -0300

    versao 2

--- app2.js
+++ app2.js
@@ -4,6 +4,9 @@ var Firebase = require("firebase");
 var fs = require('fs');
 var sys = require('util');
 var exec = require('child_process').exec, child, child1;
+var _=require('underscore');
+
+var commands = [];
 
   var config = {
     apiKey: "AIzaSyD03tfMzN7KtNrydxx73ScCDeuXhpDjZdg",
@@ -20,16 +23,29 @@ var slowTime = 10000; // Time used to check the top list and CPU usage.
 
 var ref = Firebase.database().ref('comandos');
 
-ref.on("value", function(snapshot){
-  console.log(snapshot.val());
-  child = exec(snapshot.val(), function (error, stdout, stderr) {
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  } else {
-    console.log(stdout);
-  }
-});
+ref.on("child_added", function(snapshot, prevChildKey){
+      console.log(snapshot.val());
+      commands.push(snapshot.val());
 
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    }
+);
+
+
+setInterval(function(){
+  _.each(commands, function(index, command){
+    child = exec(command.exec, function (error, stdout, stderr) {
+        if (error !== null) {
+          console.error('exec error: ' + error);
+        } else {
+          console.log(command.name + " " + stdout);
+        }
+      }
+    );
+
+
+  });
+
+
+}, 1000);
